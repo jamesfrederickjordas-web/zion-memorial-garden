0e7ec6dd7dc876fd255fab91ba767112c7cc9862
jamesfrederickjordas-web <jamesfrederickjordas@gmail.com>
2026-09-10 18:23:36 +0800

    Update register.js

--- js/register.js
+++ js/register.js
@@ -143,7 +143,7 @@ function initRegister() {
             }
 
             try {
-                const response = await fetch('http://localhost:5000/register', {
+               const response = await fetch('/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(formData)
